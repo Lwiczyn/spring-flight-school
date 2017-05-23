5c3e7d93395df5eea8e2e9bf2b5b5aa9544a3e0b
LW027880 <Louis.Wiczynski@Cerner.com>
2017-05-23 15:37:10 -0500

    added start.spring.io

---
+++ src/main/java/com/spring/flight/school/flightschool/FlightSchoolApplication.java
@@ -0,0 +1,12 @@
+package com.spring.flight.school.flightschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightSchoolApplication.class, args);
+	}
+}
